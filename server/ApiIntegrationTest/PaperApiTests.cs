57967c407c005850e70c990fefb87d37e29b78a7
Kimi310 <94898434+Kimi310@users.noreply.github.com>
2024-09-26 12:57:41 +0200

    paper api test created

---
+++ server/ApiIntegrationTest/PaperApiTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using API;
+using DataAccess;
+using DataAccess.Models;
+using DataAccess.TransferModels.Request;
+using Microsoft.AspNetCore.Mvc.Testing;
+using PgCtx;
+using Service.TransferModels.Responses;
+using Xunit;
+
+namespace ApiIntegrationTest;
+
+public class PaperApiTests : WebApplicationFactory<Program>
+{
+    private readonly PgCtxSetup<DunderContext> _pgCtxSetup = new();
+
+    public PaperApiTests()
+    {
+        Environment.SetEnvironmentVariable("DbConnectionString", 
+            _pgCtxSetup._postgres.GetConnectionString());
+    }
+
+    [Fact]
+    public async Task Create_Paper_Api_Test_Persist_To_Db()
+    {
+        var createPaperDto = new CreatePaperDto
+        {
+            Name = "Paper",
+            Stock = 10,
+            Price = 25
+        };
+        
+        var act = await CreateClient().PostAsJsonAsync("api/Paper/CreatePaper", createPaperDto);
+        
+        
+        var returnedPaper = JsonSerializer.Deserialize<Paper>(await act.Content.ReadAsStringAsync(),
+            new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true
+        })?? throw new InvalidOperationException();;
+        
+        var paperInDb = _pgCtxSetup.DbContextInstance.Papers.First();
+        Assert.Equal(createPaperDto.Name, returnedPaper.Name);
+        Assert.Equal(createPaperDto.Stock, returnedPaper.Stock);
+        Assert.Equal(createPaperDto.Price, returnedPaper.Price);
+        Assert.Equivalent(createPaperDto.Name, paperInDb.Name);
+        Assert.Equal(createPaperDto.Stock, paperInDb.Stock);
+        Assert.Equal(createPaperDto.Price, paperInDb.Price);
+    }
+
+}
